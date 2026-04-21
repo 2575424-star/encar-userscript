3ec2c59f4819733682cf2b337b876cfe42debc9a
2575424-star <2575424@gmail.com>
2026-04-21 17:47:35 +0400

    Update core-hub.js

--- libs/core-hub.js
+++ libs/core-hub.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Encar Core Hub
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  Центральное хранилище данных и событий
 // @match        *://www.encar.com/cars/detail/*
 // @match        *://fem.encar.com/cars/detail/*
@@ -17,34 +17,37 @@
         unsafeWindow.EncarHub = {
             // Хранилище данных
             _data: {},
-
+            
             // Подписчики на события
             _listeners: {},
-
+            
+            // Подписчики на одно событие (once)
+            _onceListeners: {},
+            
             // Установить значение
             set: function(key, value, silent = false) {
                 const oldValue = this._data[key];
                 this._data[key] = value;
-
+                
                 if (!silent) {
                     this.emit(`${key}:changed`, { key, value, oldValue });
                     this.emit('any:changed', { key, value, oldValue });
                 }
-
+                
                 console.log(`[Hub] ${key} =`, value);
                 return value;
             },
-
+            
             // Получить значение
             get: function(key) {
                 return this._data[key];
             },
-
+            
             // Получить все данные
             getAll: function() {
                 return { ...this._data };
             },
-
+            
             // Подписаться на событие
             on: function(event, callback) {
                 if (!this._listeners[event]) {
@@ -52,25 +55,52 @@
                 }
                 this._listeners[event].push(callback);
             },
-
+            
+            // Подписаться на событие один раз
+            once: function(event, callback) {
+                if (!this._onceListeners[event]) {
+                    this._onceListeners[event] = [];
+                }
+                this._onceListeners[event].push(callback);
+            },
+            
             // Отписаться
             off: function(event, callback) {
-                if (!this._listeners[event]) return;
-                this._listeners[event] = this._listeners[event].filter(cb => cb !== callback);
+                if (this._listeners[event]) {
+                    this._listeners[event] = this._listeners[event].filter(cb => cb !== callback);
+                }
+                if (this._onceListeners[event]) {
+                    this._onceListeners[event] = this._onceListeners[event].filter(cb => cb !== callback);
+                }
             },
-
+            
             // Вызвать событие
             emit: function(event, data) {
-                if (!this._listeners[event]) return;
-                this._listeners[event].forEach(callback => {
-                    try {
-                        callback(data);
-                    } catch(e) {
-                        console.error(`[Hub] Ошибка в обработчике ${event}:`, e);
-                    }
-                });
+                // Обычные подписчики
+                if (this._listeners[event]) {
+                    this._listeners[event].forEach(callback => {
+                        try {
+                            callback(data);
+                        } catch(e) {
+                            console.error(`[Hub] Ошибка в обработчике ${event}:`, e);
+                        }
+                    });
+                }
+                
+                // Подписчики once
+                if (this._onceListeners[event]) {
+                    const callbacks = [...this._onceListeners[event]];
+                    delete this._onceListeners[event];
+                    callbacks.forEach(callback => {
+                        try {
+                            callback(data);
+                        } catch(e) {
+                            console.error(`[Hub] Ошибка в once-обработчике ${event}:`, e);
+                        }
+                    });
+                }
             },
-
+            
             // Ждать событие (Promise)
             waitFor: function(key, timeout = 10000) {
                 return new Promise((resolve, reject) => {
@@ -79,24 +109,24 @@
                         resolve(existing);
                         return;
                     }
-
+                    
                     const timeoutId = setTimeout(() => {
                         this.off(`${key}:changed`, handler);
                         reject(new Error(`Timeout waiting for ${key}`));
                     }, timeout);
-
+                    
                     const handler = (data) => {
                         clearTimeout(timeoutId);
                         this.off(`${key}:changed`, handler);
                         resolve(data.value);
                     };
-
+                    
                     this.on(`${key}:changed`, handler);
                 });
             }
         };
     }
-
+    
     // Добавляем стили для панели (глобально)
     GM_addStyle(`
         #encar-combined-panel,
@@ -105,10 +135,10 @@
             -webkit-translate: no !important;
         }
     `);
-
+    
     // Защита от Google Translate
     document.documentElement.setAttribute('translate', 'no');
     document.body.setAttribute('translate', 'no');
-
-    console.log('[CoreHub] Инициализирован');
+    
+    console.log('[CoreHub] Инициализирован (версия 2.0)');
 })();
